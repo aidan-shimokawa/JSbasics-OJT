8c7eb4b2a7e65f3ff49720534b05befce7cb19e7
Aidan <aidanshimokawa21@gmail.com>
2023-01-24 15:41:58 +0800

    Update

---
+++ ans.js
@@ -0,0 +1,64 @@
+2 == '2'
+// true
+'he' == 'she'
+// false
+2 === 2
+// true
+'true' == true
+// true
+true === true
+// true
+'true' != true
+// true
+'true' !== true
+// true
+// 3 different variables 
+//var- has a global scope
+//let- block scope
+//const- block scope, permanet 
+
+const pets = () => {
+    let cat = 'cat'
+const dog = 2 
+console.log(  2 + dog)
+
+}
+
+// pets()
+
+//map method
+// const pets= [1, 2, 3];
+
+// // add string after each final participant
+// const announcements = pets.map(member => {
+//     // map() - creates a new set of array
+//   return member += 1;
+// })
+
+// console.log(announcements);
+
+// call back functions
+// let name = 'John'
+// function greeting(name) {
+//     alert(`Hello, ${name}`);
+//   }
+  
+//   function processUserInput(callback) {
+//     const name = prompt("Please enter your name.");
+//     callback(name);
+//   }
+// For loop 
+// const someArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let text= "";
+// for (let i = 0; i < someArray.length; i++) {
+//   text += someArray[i] + " ";
+// }
+// console.log(text)
+// concat
+//const someArray = [1, 2, 3];
+// const someArray2 = [ 4, 5];
+// let  result = someArray.concat(someArray2);
+// console.log(result) 
+  
+
